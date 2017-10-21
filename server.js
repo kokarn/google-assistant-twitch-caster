7aad749baf2848ef9367dc01cee518ed7347dedd
Oskar Risberg <oskar.risberg@stendahls.se>
2017-10-21 21:07:51 +0200

    Include the working example instead of just echoing things

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const chromecasts = require( 'chromecasts' )();
 
 const PLAY_TARGET_NAME = '';
 const HOME_TARGET = '';
+const RECONNECT_DELAY = 5000;
+const TARGET_QUALITY = '720p';
 
 let ws;
 
@@ -18,8 +20,16 @@ const sayThing = function sayThing ( thing ) {
     } );
 }
 
-const playStream = function playStream ( streamURI ) {
-    playTarget.play( streamURI );
+const playStream = function playStream ( streams ) {
+    for ( let i = 0; i < streams.length; i = i + 1 ) {
+        if ( streams[ i ].quality.toLowerCase() === TARGET_QUALITY.toLowerCase() ) {
+            playTarget.play( streams[ i ].uri );
+
+            return true;
+        }
+    }
+
+    console.error( `Unable to find a stream with the preferred quality ( ${ TARGET_QUALITY } )` );
 };
 
 const connectToServer = function connectToServer () {
@@ -32,7 +42,7 @@ const connectToServer = function connectToServer () {
 
     ws.on( 'close', () => {
         console.log( 'Disconnected from server, reconnecting in 5 seconds' );
-        setTimeout( connectToServer, 5000 );
+        setTimeout( connectToServer, RECONNECT_DELAY );
     } );
 
     ws.on( 'error', ( socketError ) => {
@@ -42,11 +52,9 @@ const connectToServer = function connectToServer () {
     ws.on( 'message', ( data ) => {
         const message = JSON.parse( data );
         if ( message.type === 'message' ) {
-            // sayThing( message.content );
-            console.log( message );
+            sayThing( message.content );
         } else if ( message.type === 'play' ) {
-            // playStream( message.content );
-            console.log( message );
+            playStream( message.content );
         } else {
             console.error( message );
         }
